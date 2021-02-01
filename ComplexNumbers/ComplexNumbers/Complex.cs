3c92e9a81a5fddeafb76c4a9cea522041f241c98
Artsem Lebedev <lebedev20211@gmail.com>
2021-02-01 23:30:59 +0300

    ComplexNumbers

---
+++ ComplexNumbers/ComplexNumbers/Complex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Complex
+    {
+        public int rez, imz;
+        public Complex(int rez, int imz)
+        {
+            this.rez = rez;
+            this.imz = imz;
+        }
+
+        public void Add(Complex number)
+        {
+            rez += number.rez;
+            imz += number.imz;
+        }
+
+        public void Subtraction(Complex number)
+        {
+            rez -= number.rez;
+            imz -= number.imz;
+        }
+
+        public void Multiply(Complex number)
+        {
+            rez = rez * number.rez - imz * number.imz;
+            imz = rez * number.imz + number.rez * imz;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"({rez},{imz})");
+        }
+    }
+}
---
+++ ComplexNumbers/ComplexNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(1, 2);
+            Complex b = new Complex(2, 2);
+            a.Print();
+            a.Add(b);
+            a.Print();
+            a.Subtraction(b);
+            a.Print();
+            a.Multiply(b);
+            a.Print();
+            b.Print();
+        }
+    }
+}
